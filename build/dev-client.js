7db13cc87d4a1d80ecf0fc4a03ea15ca6e2af760
niepengfei@haier.com <niepengfei@haier.com>
2021-11-02 17:00:29 +0800

    删除之前代码

--- build/dev-client.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-require('eventsource-polyfill')
-var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true')
-// 将所有entry文件添加热更新 hot-reload
-hotClient.subscribe(function (event) {
-  if (event.action === 'reload') {
-    window.location.reload()
-  }
-})
